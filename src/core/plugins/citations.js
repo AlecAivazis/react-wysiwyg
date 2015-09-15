d38ba3bb30d2c362b6700f655e87523e2d007035
Alec Aivazis <alec@aivazis.com>
2015-09-15 00:12:21 -0500

    more work

--- src/core/plugins/citations.js
+++ src/core/plugins/citations.js
@@ -1,4 +1,10 @@
 // a CKEditor widget for displaying citations
+
+const element_template = '<cite class="au-cite">' + 
+                            'foo' +
+                         '</cite>'
+
+// export the widget
 export default {
     requires: 'widget,dialog',
 
@@ -7,7 +13,7 @@ export default {
         // add the widget to the editor
         editor.widgets.add('citation', {
             // the html to display for the widget
-            template: '<cite class="au-cite">foo</cite>',
+            template: element_template,
             inline: true,
             // check if an element is a citation
             upcast: (element) => {
@@ -18,19 +24,32 @@ export default {
             },
             // called when the widget recieves new data
             data: function() {
+                // save a reference to the cite element
+                let cite_element = this.parts.cite_text
                 // set the inner html of the cite element
-                this.parts.cite_text.setHtml(this.data.text)
+                cite_element.setHtml(this.data.text || '..')
+                cite_element.data('bib-text', this.data.text)
             },
             edit: function() {
 
-                const text = prompt('insert text')
-                if (text) {
-                    // set the widget data
-                    this.setData({text: text})
-                } else {
-                    return false
+                // the callback that creates a citation out of the data from the modal
+                const insert_callback = (bib_key, bib_text, type, display_text) => {
+                    console.log('calling callback')
+                    // use the bib key if there is no display text
+                    display_text = display_text || bib_key
+
+                    console.log('updating state')
+                    this.setData({text: display_text})
                 }
-            }
+
+                // save a reference to the element for the widget
+                const element = this.element
+                // save the bib text
+                const bibText = element.data('bib-text')
+
+                // open the citation modal 
+                open_citation_modal()
+            },
         })
     }
 }
